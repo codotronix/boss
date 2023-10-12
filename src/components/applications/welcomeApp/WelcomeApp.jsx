30274b6875ba5942ad31f897db412f895bc6a0f8
Suman Barick <codotronix@gmail.com>
2023-10-13 03:19:18 +0530

    minor update in demo app

--- src/components/applications/welcomeApp/WelcomeApp.jsx
+++ src/components/applications/welcomeApp/WelcomeApp.jsx
@@ -7,6 +7,7 @@ const WelcomeApp = (props) => {
         <div className={styles.root}>
             <i className="fa-solid fa-face-smile"></i>
             <h2>Welcome to B.O.S.S</h2>
+            <div><u>B</u>asic <u>OS</u> <u>S</u>imulation</div>
         </div>
     )
 }
