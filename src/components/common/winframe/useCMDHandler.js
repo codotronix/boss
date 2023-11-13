a678795121126b0ca165c5f05aad129bd5f08259
Suman Barick <codotronix@gmail.com>
2023-11-13 22:40:33 +0530

    wrote commnets for useCMDHandler hook

--- src/components/common/winframe/useCMDHandler.js
+++ src/components/common/winframe/useCMDHandler.js
@@ -1,16 +1,24 @@
 import { useRef, useEffect } from "react";
 
-export function useCMDHandler(cmd, handlerFn) {
-    const cmdRef = useRef('')
+/**
+ * Whenever user clicks on any menu-command in WinFrame,
+ * The WinFrame passes `menuCommand` in props to the AppComponent
+ * @param {String} menuCommand | The Menu Command to Watch for Change
+ * @param {*} handlerFn | The callback function to call when new Command is received
+ */
+export function useCMDHandler(menuCommand, handlerFn) {
+    const cmdRef = useRef('')   // to store already handled command
 
     // Watch the menuCommand
     // Whenever user clicks on the menu, it will get triggered
     useEffect(() => {
-        // already handled value?
-        if(cmd !== cmdRef.current) {
-            handlerFn(cmd)
-            cmdRef.current = cmd
+        // New value?
+        if(menuCommand !== cmdRef.current) {
+            // handle it by calling the callback fn
+            handlerFn(menuCommand)
+            // mark it as handled
+            cmdRef.current = menuCommand
         }
     }, 
-    [cmd, handlerFn])
+    [menuCommand, handlerFn])
 }
