8f8910414d0c30798c7b38068f2c9f59035da30d
Suman Barick <codotronix@gmail.com>
2023-11-15 01:30:43 +0530

    placeholder component for BinApp

--- src/App.test.js
+++ src/App.test.js
@@ -18,7 +18,7 @@ describe('App', () => {
         expect(dock).toBeInTheDocument()
     })
 
-    it('should render Dock with all the buttons', () => {
+    it('should render Dock with all the Docked Apps', () => {
         render(<App />)
         let dockedAppsNames = Object.values(APPS_DETAILS).filter(a => a.docked).map(a => a.name)
         // For each docked app
@@ -32,7 +32,7 @@ describe('App', () => {
         }
     })
 
-    it('should open apps window if cliked on apps in dock', () => {
+    it('should open apps windows if cliked on apps in Dock', () => {
         const { container } = render(<App />)
         const appsBtn = screen.getByRole('button', {
             name: 'Apps'
@@ -53,7 +53,7 @@ describe('App', () => {
         expect(winFrames.length).toBe(1)
     })
 
-    it('should open multiple instances of applications on multiple clicks', () => {
+    it('should open multiple instances on multiple clicks on Dock Apps', () => {
         const { container } = render(<App />)
         const foldersBtn = screen.getByRole('button', {
             name: 'Folders'
@@ -76,7 +76,7 @@ describe('App', () => {
         expect(currentWinFrameCount-previousWinFrameCount).toBe(3)
     })
 
-    it('WinFrame close buttons should close the applications', () => {
+    it('WinFrame close button should close the application', () => {
         const { container } = render(<App />)
         // let's create some winframes
         // i.e. open some applications
@@ -106,4 +106,34 @@ describe('App', () => {
         expect(winframesCount).toBe(0)
     })
 
+    // NOTE
+    // THIS SHOULD BE TESTED WHEN NO APPS ARE OPEN,
+    // BECAUSE SINGLE-INSTANCE APP, IF ALREADY OPEN, WON'T OPEN AGAIN...
+    // IT CAN BE DONE HERE AS WE JUST CLOSED ALL THE APPS IN THE PREVIOUS TEST
+    it('all the apps in the dock should open when clicked', () => {
+        const { container } = render(<App />)
+        let dockedAppsNames = Object.values(APPS_DETAILS).filter(a => a.docked).map(a => a.name)
+
+        // how many WinFrames were already open?
+        const previousWinFrameCount = container.querySelectorAll('.WinFrame').length
+
+        // For each docked app
+        // there should be a button in the Dock
+        // Click it
+        for (let da of dockedAppsNames) {
+            const btn = screen.getByRole('button', {
+                name: da
+            })
+
+            expect(btn).toBeInTheDocument()
+            act(() => userEvent.click(btn))
+        }
+
+        // total how many winFrames are now open
+        const currentWinFrameCount = container.querySelectorAll('.WinFrame').length
+
+        // The differences should be exactly the number of apps
+        // in the docks that we just clicked to open
+        expect(currentWinFrameCount-previousWinFrameCount).toBe(dockedAppsNames.length)
+    })
 })
