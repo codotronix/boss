be57755efdb52bdffb189c8cd9dbaf958cd2685b
Suman Barick <codotronix@gmail.com>
2025-12-28 00:21:37 +0530

    formatting fixed

--- v1/src/const/MAP_APP_ID_TO_COMPONENT.ts
+++ v1/src/const/MAP_APP_ID_TO_COMPONENT.ts
@@ -1,13 +1,21 @@
-import { AppsViewerApp, BCalc, BinApp, FoldersApp, SettingsApp, TerminalApp, TimerApp, WelcomeApp
-} from '../components/applications'
+import {
+  AppsViewerApp,
+  BCalc,
+  BinApp,
+  FoldersApp,
+  SettingsApp,
+  TerminalApp,
+  TimerApp,
+  WelcomeApp,
+} from "../components/applications";
 
 export const MAP_APP_ID_TO_COMPONENT = {
-    "appsviewer": AppsViewerApp,
-    "bcalc": BCalc,
-    "bin": BinApp,
-    "folders": FoldersApp,
-    'settings': SettingsApp,
-    "terminal": TerminalApp,
-    "timer": TimerApp,
-    "welcomeapp": WelcomeApp,
-}
+  appsviewer: AppsViewerApp,
+  bcalc: BCalc,
+  bin: BinApp,
+  folders: FoldersApp,
+  settings: SettingsApp,
+  terminal: TerminalApp,
+  timer: TimerApp,
+  welcomeapp: WelcomeApp,
+};
